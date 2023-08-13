30b3dfb1d7da9152f5127db6d090cd20b7fb259a
Yasin1406 <bsse1406@iit.du.ac.bd>
2023-08-14 02:21:17 +0600

    aug 14

---
+++ ols.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include "matrix_operations.cpp"
+using namespace std;
+
+vector<vector<double>> X;
+vector<double> Y;
+int m,n;
+
+int main(){
+    int i,j;
+    cout<<"Enter number of rows and columns: ";
+    cin>>m>>n;
+    X.resize(m,vector<double>(n));
+    Y.resize(n);
+    for(i=0;i<)
+}
