f69c1660a12bd133ee95ec890eef3bf20be3ab87
Yasin1406 <bsse1406@iit.du.ac.bd>
2023-12-15 18:52:44 +0600

    a

--- Runner/Main.cpp
+++ Runner/Main.cpp
@@ -35,10 +35,10 @@ int main()
         temp.clear();
         kaczmarz_solution.clear();
         ols_solution.clear();
-        cout<<"Enter size of the matrix: ";
+        cout<<endl<<"Enter size of the matrix: ";
         cin>>n;
     
-        cout<<"Enter the augmented matrix:"<<endl;
+        cout<<endl<<"Enter the augmented matrix:"<<endl;
         for(i=0;i<n;i++)
         {
             for(j=0;j<n;j++)
@@ -51,10 +51,10 @@ int main()
             cin>>value;
             cons.push_back(value);
         }
-        cout<<"Enter number of iterations for kaczmarz operation: ";
+        cout<<endl<<"Enter number of iterations for kaczmarz operation: ";
         cin>>iter;
         kaczmarz_operation(mat,cons,kaczmarz_solution,iter);
-        cout<<"Solution using Kaczmarz:"<<endl;
+        cout<<endl<<"Solution using Kaczmarz:"<<endl;
         for(auto s:kaczmarz_solution)
         {
             printf("%.3lf\n",s);
@@ -73,20 +73,60 @@ int main()
             cout<<"There is 'significant differences' between the solutions of Kaczmarz method and OLS method ";
             cout<<"when using "<<iter<<" iterations"<<endl;
         }
-        else{
+        else
+        {
             cout<<"There is 'no significant difference' between the solutions of Kaczmarz method and OLS method ";
             cout<<"when using "<<iter<<" iterations"<<endl;
         }
         cout<<endl;
-        cout<<"Choices:"<<endl;
+        cout<<"Do you want to solve the same system for different iteration?"<<endl;
+        cout<<"Enter 1 for yes. For no enter anything else."<<endl;
+        cout<<endl<<"Enter your choice: ";
+        cin>>choice;
+        while(choice==1)
+        {
+            cout<<endl<<"Enter number of iterations for kaczmarz operation: ";
+            cin>>iter;
+            kaczmarz_solution.clear();
+            kaczmarz_operation(mat,cons,kaczmarz_solution,iter);
+            cout<<endl<<"Solution using Kaczmarz:"<<endl;
+            for(auto s:kaczmarz_solution)
+            {
+                printf("%.3lf\n",s);
+            }
+            cout<<endl<<endl;
+            cout<<"Solution using OLS:"<<endl;
+            for(auto s:ols_solution)
+            {
+                printf("%.3lf\n",s);
+            }
+            cout<<endl<<endl;
+            detect_outlier(kaczmarz_solution,ols_solution,outliers);
+            if(outliers.size()>0)
+            {
+                cout<<"There is 'significant differences' between the solutions of Kaczmarz method and OLS method ";
+                cout<<"when using "<<iter<<" iterations"<<endl;
+            }
+            else
+            {
+                cout<<"There is 'no significant difference' between the solutions of Kaczmarz method and OLS method ";
+                cout<<"when using "<<iter<<" iterations"<<endl;
+            }
+            cout<<endl<<endl;
+            cout<<"Do you want to solve the same system for different iteration?"<<endl;
+            cout<<"Enter 1 for yes. For no enter anything else."<<endl;
+            cout<<endl<<"Enter your choice: ";
+            cin>>choice;
+        }
+        cout<<endl<<"Choices:"<<endl;
         cout<<"1. Enter solution process"<<endl;
         cout<<"2. Quit"<<endl<<endl;
-        cout<<"Enter your choice: ";
+        cout<<endl<<"Enter your choice: ";
         cin>>choice;
         while(choice!=1&&choice!=2)
         {
             cout<<"Invalid choice!"<<endl;
-            cout<<"Please enter your choice again: ";
+            cout<<endl<<"Please enter your choice again: ";
             cin>>choice;
         }
     }
